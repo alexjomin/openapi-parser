02f96748062514408fd28bb82064962f9b6ade34
Jeffrey Duroyon <jduroyon@norauto.com>
2018-03-16 01:12:31 +0100

    add unitary test

---
+++ docparser/parser_test.go
@@ -0,0 +1,151 @@
+package docparser
+
+import (
+	"go/ast"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type parseJSONTagTestCase struct {
+	description     string
+	field           *ast.Field
+	expectedJSONTag jsonTagInfo
+	expectedError   string
+}
+
+func TestParseJSONTag(t *testing.T) {
+	testCases := []parseJSONTagTestCase{
+		{
+			description:     "Should not set name in jsontag",
+			field:           &ast.Field{},
+			expectedJSONTag: jsonTagInfo{},
+		},
+		{
+			description: "Should set name in jsontag",
+			field: &ast.Field{
+				Names: []*ast.Ident{
+					&ast.Ident{
+						Name: "testName",
+					},
+				}},
+			expectedJSONTag: jsonTagInfo{
+				name: "testName"},
+		},
+		{
+			description: "Should set firstname in jsontag",
+			field: &ast.Field{
+				Names: []*ast.Ident{
+					&ast.Ident{
+						Name: "firstTestName",
+					},
+					&ast.Ident{
+						Name: "secondTestName",
+					},
+				}},
+			expectedJSONTag: jsonTagInfo{
+				name: "firstTestName"},
+		},
+		{
+			description: "Should return empty jsonTagInfo",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{},
+		},
+		{
+			description: "Should fail with invalid syntax in tag value",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "Test",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{},
+			expectedError:   "invalid syntax",
+		},
+		{
+			description: "Should parse tag value",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "`Test`",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{},
+		},
+		{
+			description: "Should parse json tag value with value -  ",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "`json:\"-\"`",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{
+				ignore:   true,
+				required: false,
+			},
+		},
+		{
+			description: "Should parse json tag value with value jsontagname",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "`json:\"jsontagname\"`",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{
+				name: "jsontagname",
+			},
+		},
+		{
+			description: "Should parse json tag value and validate",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "`json:\"jsontagname\" validate:\"required\"`",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{
+				required: true,
+				name:     "jsontagname",
+			},
+		},
+		{
+			description: "Should parse json tag value and validate with enum",
+			field: &ast.Field{
+				Tag: &ast.BasicLit{
+					ValuePos: 0,
+					Kind:     0,
+					Value:    "`json:\"jsontagname\" validate:\"required,enum=a b\"`",
+				},
+			},
+			expectedJSONTag: jsonTagInfo{
+				required: true,
+				name:     "jsontagname",
+				enum:     []string{"a", "b"},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			parsedJSONTag, err := parseJSONTag(tc.field)
+			if tc.expectedError != "" {
+				assert.Equal(t, tc.expectedError, err.Error())
+				return
+			}
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedJSONTag, parsedJSONTag)
+		})
+	}
+}
